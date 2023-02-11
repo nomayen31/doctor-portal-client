e3fc81f292451d83443500bb34d9e20d024a2ecd
nomayen <nomayen.ohin@gmail.com>
2023-02-11 19:14:26 +0600

    home and navbar 1

--- src/pages/Home/Banner/Banner.js
+++ src/pages/Home/Banner/Banner.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import './Banner.css'
+import IMG1 from '.././../../assets/images/chair.png' 
 
 const Banner = () => {
     return (
-        <div>
-            
+        <div className="hero">
+            <div className="hero-content flex-col lg:flex-row-reverse">
+                <img src={IMG1} className=" rounded-lg w-1/2 shadow-2xl" alt='' />
+                <div>
+                    <h1 className="text-5xl font-bold">Box Office News!</h1>
+                    <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                    <button className="btn btn-primary bg-gradient-to-r from-primary to-secondary text-white">Get Started</button>
+                </div>
+            </div>
         </div>
     );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,27 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
+  daisyui: {
+    themes: [
+      {
+        doctorTheme: {
+          primary: "#0FCFEC",
+          secondary: "#19D3AE",
+          accent: "#3A4256",
+          neutral: "#3D4451",
+          "base-100": "#FFFFFF",
+
+          // "info": "#53C0F3",
+
+          // "success": "#71EAD2",
+
+          // "warning": "#F3CC30",
+
+          // "error": "#E24056",
+        }
+      }
+    ]
+  },
   theme: {
     extend: {},
   },
