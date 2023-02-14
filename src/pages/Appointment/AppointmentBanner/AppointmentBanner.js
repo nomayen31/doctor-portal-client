a9992592eee44cd64373e2fbbcf545a5e5e9e298
nomayen <nomayen.ohin@gmail.com>
2023-02-14 21:08:27 +0600

    DayPickr

--- src/pages/Appointment/AppointmentBanner/AppointmentBanner.js
+++ src/pages/Appointment/AppointmentBanner/AppointmentBanner.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import chiar1 from '../../../assets/images/chair.png'
+import { DayPicker } from 'react-day-picker';
 
 const AppointmentBanner = () => {
     return (
@@ -8,7 +9,7 @@ const AppointmentBanner = () => {
                 <div className="hero-content flex-col lg:flex-row-reverse">
                     <img src={chiar1} alt="" className="max-w-sm rounded-lg shadow-2xl" />
                     <div>
-                        
+                        <DayPicker></DayPicker>
                     </div>
                 </div>
             </div>
