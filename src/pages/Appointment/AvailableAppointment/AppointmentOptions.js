86382eb673634f662cce9145e675b7b716f4deea
nomayen <nomayen.ohin@gmail.com>
2023-02-15 11:01:25 +0600

    appointmentOptions1

---
+++ src/pages/Appointment/AvailableAppointment/AppointmentOptions.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const AppointmentOptions = ({ appointmentOptions }) => {
+
+    const { name, slots } = appointmentOptions;
+    return (
+        <div className="card shadow-xl">
+            <div className="card-body text-center ">
+                <h2 className="text-2xl text-secondary font-bold text-center">{name}</h2>
+                <p>{slots.length > 0 ? slots[0] : 'Try another day'}</p>
+                <p>{slots.length} {slots.length > 1? 'Spaces' : 'Space'}</p>
+                <div className="card-actions justify-center">
+                    <button className="btn btn-primary text-white">Book Appointment</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default AppointmentOptions;
--- src/pages/Appointment/AvailableAppointment/AvailableAppointment.js
+++ src/pages/Appointment/AvailableAppointment/AvailableAppointment.js
@@ -1,10 +1,27 @@
 import { format } from 'date-fns';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import AppointmentOptions from './AppointmentOptions';
 
 const AvailableAppointment = ({selectedDate}) => {
+    const [appointmentOptions, setAppointmentOptions] =useState([])
+
+    useEffect(() =>{
+        fetch('appointmentOptions.json')
+        .then(res => res.json())
+        .then(data =>setAppointmentOptions(data));
+    },[])
     return (
-        <section className='mt-16'>
+        <section className='my-16'>
             <p className='text-center text-secondary font-bold'>Available Appointment on {format(selectedDate, 'PP')} </p>
+            <div className='grid gap-5 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-6'>
+                {
+                    appointmentOptions.map(option => <AppointmentOptions
+                    key={option._id}
+                    appointmentOptions={option}
+                    
+                    ></AppointmentOptions>)
+                }
+            </div>
         </section>
     );
 };
