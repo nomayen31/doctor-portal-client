5172532f8a10ecc0f0c9e25d1c79da5961aeb431
nomayen <nomayen.ohin@gmail.com>
2023-02-15 21:39:54 +0600

    date add on form

--- src/pages/Appointment/BookingModal/BookingModal.js
+++ src/pages/Appointment/BookingModal/BookingModal.js
@@ -1,7 +1,9 @@
+import { format } from 'date-fns';
 import React from 'react';
 
-const BookingModal = ({treatment}) => {
+const BookingModal = ({treatment, selectedDate}) => {
     const {name} =treatment;
+    const date =format(selectedDate, 'PP')
     return (
         <>
             <input type="checkbox" id="booking-modal" className="modal-toggle" />
@@ -9,7 +11,15 @@ const BookingModal = ({treatment}) => {
                 <div className="modal-box relative">
                     <label htmlFor="booking-modal" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
                     <h3 className="text-lg font-bold">{name}</h3>
-                    <p className="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+                    <form className='grid grid-col-1 gap-3 mt-10'>
+                    <input type="text" disabled value={date} className="input w-full input-bordered " />
+                    <input type="text" placeholder="Type here" className="input w-full input-bordered " />
+                    <input type="text" placeholder="Type here" className="input w-full input-bordered " />
+                    <input type="text" placeholder="Type here" className="input w-full input-bordered " />
+                    <input type="text" placeholder="Type here" className="input w-full input-bordered " />
+                    <br></br>
+                    <input className='btn btn-accent w-full input-bordered max-w-xm' type="submit" value="submit"></input>
+                    </form>
                 </div>
             </div>
         </>
