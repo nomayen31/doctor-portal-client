bb09a1fc77eb3b1073947c6c42c44e61501ad9f0
nomayen <nomayen.ohin@gmail.com>
2023-03-01 18:34:29 +0600

    font to server data add booking

--- src/pages/Appointment/BookingModal/BookingModal.js
+++ src/pages/Appointment/BookingModal/BookingModal.js
@@ -1,29 +1,44 @@
 import { format } from 'date-fns';
 import React, { useContext } from 'react';
+import { toast } from 'react-hot-toast';
 import { AuthContext } from '../../../context/AuthProvider';
 
-const BookingModal = ({ treatment,setTreatment ,selectedDate }) => {
+const BookingModal = ({ treatment, setTreatment, selectedDate }) => {
     const { name, slots } = treatment;
     const date = format(selectedDate, 'PP')
-    const{user} =useContext(AuthContext)
+    const { user } = useContext(AuthContext)
 
-    const handleBooking = e=>{
+    const handleBooking = e => {
         e.preventDefault();
-        const form =e.target;
-        const slot =form.slot.value;
-        const name= form.name.value;
-        const email =form.email.value;
-        [3,4,5].map((value, i) => console.log(value));
-        const phone =form.phone.value;
-        const booking={
-          appointmentDate: date,
-          treatment:name,
-           patent: name,
+        const form = e.target;
+        const slot = form.slot.value;
+        const name = form.name.value;
+        const email = form.email.value;
+        [3, 4, 5].map((value, i) => console.log(value));
+        const phone = form.phone.value;
+        const booking = {
+            appointmentDate: date,
+            treatment: name,
+            patent: name,
             slot,
             email,
             phone,
         }
-        console.log(booking);
+        fetch('http://localhost:5000/bookings', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(booking)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if (data.acknowledged) {
+                    setTreatment(null);
+                    toast.success('booking confirmed')
+                }
+            })
         setTreatment(null);
 
     }
@@ -37,21 +52,21 @@ const BookingModal = ({ treatment,setTreatment ,selectedDate }) => {
                     <form onSubmit={handleBooking} className='grid grid-col-1 gap-3 mt-10'>
                         <input type="text" disabled value={date} className="input w-full input-bordered " />
                         <select name='slot' className="select select-bordered w-full ">
-                            
+
                             {
-                                slots.map((slot, i) => <option 
-                                value={slot}
-                                key={i}
+                                slots.map((slot, i) => <option
+                                    value={slot}
+                                    key={i}
                                 >{slot}</option>)
                             }
                         </select>
                         <input name="name" type="text"
-                        defaultValue={user?.displayName} disabled
-                        placeholder="Your Name" className="input w-full input-bordered" />
-                        <input name="email" type="email" 
-                        defaultValue={user?.email}
-                        disabled
-                        placeholder="Email Address" className="input w-full input-bordered" />
+                            defaultValue={user?.displayName} disabled
+                            placeholder="Your Name" className="input w-full input-bordered" />
+                        <input name="email" type="email"
+                            defaultValue={user?.email}
+                            disabled
+                            placeholder="Email Address" className="input w-full input-bordered" />
                         <input name="phone" type="text" placeholder="Phone Number" className="input w-full input-bordered" />
                         <br />
                         <input className='btn btn-accent w-full' type="submit" value="Submit" />
