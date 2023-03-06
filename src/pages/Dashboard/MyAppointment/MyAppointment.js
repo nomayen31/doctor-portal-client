370b3a3268fd4fba09a28c9ae327d0c35a4a00d7
nomayen <nomayen.ohin@gmail.com>
2023-03-06 20:59:26 +0600

    table create and data lode

--- src/pages/Dashboard/MyAppointment/MyAppointment.js
+++ src/pages/Dashboard/MyAppointment/MyAppointment.js
@@ -6,9 +6,14 @@ import {useQuery} from '@tanstack/react-query'
 const MyAppointment = () => {
   const {user} =useContext(AuthContext);
 
-  const url =`http://localhost:5000/bookings?email = ${user?.email}`;
-  const {} =useQuery({
-    queryKey:['bookings']
+  const url =`http://localhost:5000/bookings?email=${user?.email}`;
+  const {data: bookings =[]} =useQuery({
+    queryKey:['bookings', user?.email],
+    queryFn: async () =>{
+      const res =await fetch(url);
+      const data =await res.json();
+      return data;
+    }
   })
     return (
         <div>
@@ -20,32 +25,21 @@ const MyAppointment = () => {
       <tr>
         <th></th>
         <th>Name</th>
-        <th>Job</th>
-        <th>Favorite Color</th>
+        <th>Treatment</th>
+        <th>Date</th>
+        <th>Time</th>
       </tr>
     </thead>
     <tbody>
-      {/* row 1 */}
-      <tr>
-        <th>1</th>
-        <td>Cy Ganderton</td>
-        <td>Quality Control Specialist</td>
-        <td>Blue</td>
-      </tr>
-      {/* row 2 */}
-      <tr className="hover">
-        <th>2</th>
-        <td>Hart Hagerty</td>
-        <td>Desktop Support Technician</td>
-        <td>Purple</td>
-      </tr>
-      {/* row 3 */}
-      <tr>
-        <th>3</th>
-        <td>Brice Swyre</td>
-        <td>Tax Accountant</td>
-        <td>Red</td>
-      </tr>
+      {
+        bookings.map((booking, i) =><tr>
+          <th>{i}</th>
+          <td>Cy Ganderton</td>
+          <td>Quality Control Specialist</td>
+          <td>Blue</td>
+          <td>Blue</td>
+        </tr>)
+      }
     </tbody>
   </table>
 </div> 
